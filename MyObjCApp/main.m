4d2d0274b332575e852943788959cd939872326d
Yagi <straitwalk@gmail.com>
2012-03-28 01:55:43 +0900

    first commit

---
+++ MyObjCApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MyObjCApp
+//
+//  Created by 夏樹 八木 on 12/03/28.
+//  Copyright 2012年 インタープリズム株式会社. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+  // insert code here...
+  NSLog(@"Hello, World!");
+
+  [pool drain];
+    return 0;
+}
+
